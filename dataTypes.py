0fabe55c11a674db04efee4747a1c88b15e473a1
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-18 23:47:02 +0530

    add: expand dataTypes.py with string manipulation examples and concatenation

--- dataTypes.py
+++ dataTypes.py
@@ -10,3 +10,22 @@ power2=10**3
 print(power, power2)
 
 lastName="Jadon"
+
+# Learning strings
+string="My Name is Vijay"
+
+my_long_string="""
+My name is vijay
+and last name is jadon
+"""
+
+firstName="John"
+lastName="Doe"
+fullName=firstName + " " + lastName #Concatenation of string
+
+
+long_dash="-"*10
+long_dash=long_dash*30
+length=long_dash.__len__()
+len(long_dash) #len is a built in function in python which calculates the length of the variable
+len(fullName)
