6708037c9382c647a136ae63f235b7bc82a1af4b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-18 23:27:34 +0530

    add: create comment.py to demonstrate single and multi-line comments in Python

---
+++ comment.py
@@ -0,0 +1,12 @@
+#here by using hashtag we comment the line
+
+score=10
+name="vijay"
+
+print(score,name) #the commented part will not print
+
+"""
+Now this is another way of 
+writting multi line comment
+"""
+
