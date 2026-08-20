3e20568033374c01e9a51777bc1488bebb2b8f5a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-20 23:12:06 +0530

    update logic_operators.py to correct has_license value and enhance logical operation examples

--- logic_operators.py
+++ logic_operators.py
@@ -1,7 +1,13 @@
 age=25
-has_license=False
+has_license=True
+drunk=False
 
 can_drive = age>=16 and has_license
 print(can_drive)
 
 can_drive = age>=16 or has_license #true
+
+print(not True)
+print(not False)
+
+can_drive=age>=18 and has_license and not drunk
