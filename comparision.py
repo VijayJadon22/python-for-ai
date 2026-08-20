715b6e2f6b0bedab224201067a5792c3d6fef729
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-20 22:58:52 +0530

    add: create comparision.py to demonstrate age comparison operations in Python

---
+++ comparision.py
@@ -0,0 +1,12 @@
+age=18
+age=16
+
+can_drive=age>=18
+print(can_drive)
+
+print(age==25) #false
+print(age!=30) #true
+print(age>20) #false
+print(age<30) #true
+print(age>=25) #false 
+print(age<=25) #true
