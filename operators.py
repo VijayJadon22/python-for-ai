3b5a2359986e611636973cb5f024a4533e19a72e
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-20 23:03:31 +0530

    add: create operators.py to demonstrate basic math and special operators in Python

---
+++ operators.py
@@ -0,0 +1,14 @@
+
+result=2+3*4 #14 not 20!
+result=(2+3)*4
+
+#Basic math operators
+print(10+3) #13
+print(10-3) #7
+print(10*3) #30
+print(10/3) #3.333
+
+#special operators
+print(10//3) #3 - Floor division
+print(10%3) #1 - Modulo operator
+print(10**3) #1000 Exponent
