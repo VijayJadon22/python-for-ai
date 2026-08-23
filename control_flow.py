6c2653c4889f933496aca17f4f44f2cd0dc46eae
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-23 13:33:18 +0530

    add: extend control_flow.py with score evaluation logic

--- control_flow.py
+++ control_flow.py
@@ -6,3 +6,17 @@ elif temperature==25:
     print("Its Okay Okay!")
 else:
     print("Its pleasant weather")
+
+
+score=86
+
+if score>=95:
+    print("Excellent!")
+elif score>=90:
+    print("Very Good!")
+elif score>=80:
+    print("Good")
+elif (score>=70):
+    print("Satisfactory!")
+else:
+    print("Work hard!")
