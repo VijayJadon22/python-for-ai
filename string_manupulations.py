7b7e34cdcb6464f104793337ac8e69dbcc52d4b3
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-23 13:07:19 +0530

    update string_methods.py to add full name construction and length calculations

--- string_manupulations.py
+++ string_manupulations.py
@@ -1,3 +1,8 @@
 name="Dave"
 
 string= f"Hi there my name is {name}!" #f string is similar to using string literals in js
+
+my_long_string="""
+Hey there my
+name is vijay
+"""
--- string_methods.py
+++ string_methods.py
@@ -3,3 +3,12 @@ text="Python Programming"
 print(text.lower())
 print(text.upper())
 print(text.title())
+
+first_name="vijay"
+middle_name="pratap singh"
+last_name="Jadon"
+
+full_name=first_name + " " + middle_name + " " + last_name
+
+len(full_name)
+len(first_name)
