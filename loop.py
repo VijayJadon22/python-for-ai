97e5d45f051bf08f72befb181c7b909c63651ebf
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-23 21:59:21 +0530

    refactor: comment out previous loops in loop.py for clarity

--- loop.py
+++ loop.py
@@ -4,5 +4,11 @@
 # for i in range(1,6):
 #     print(i)
 
-for i in range(0,10,2):
+# for i in range(0,10,2):
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+
+for i in range(0, 20, 2):
     print(i)
