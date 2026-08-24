e248d2b331e6661b8cae2396fa007a2054c3b488
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 19:33:40 +0530

    add: implement additional calculation functions for total price and price with tax

--- learning_functions/params.py
+++ learning_functions/params.py
@@ -25,3 +25,17 @@ def greet_default(last_name, first_name="Vijay"): #if there's no default value t
     print(f"Hello {first_name} {last_name}")
 
 greet_default("Singh")
+
+def calculate_total(price,tax_rate,discount):
+    tax=price*tax_rate
+    total=price+tax-discount
+    print(f"Total price is ${total}")
+
+calculate_total(price=100,tax_rate=0.08,discount=10)
+
+def calculate_price(price,tax,discount):
+    tax_price=price*tax/100
+    total_price=price+tax_price-discount
+    print(f"Total price of the product is {total_price}")
+
+calculate_price(1000,20,50)
