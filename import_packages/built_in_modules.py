d9dc89fbcb65ad5a7472d7d0fa69d2c6a5b8b5c0
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 22:43:05 +0530

    add: implement built_in_modules.py to demonstrate random number and choice selection

---
+++ import_packages/built_in_modules.py
@@ -0,0 +1,8 @@
+# Import entire module
+import random
+
+# Use module functions
+number=random.randint(1,10) #selecting random number between the range 1-10
+print(number)
+choice=random.choice(["apple","banana","mango","grape"])
+print(choice)
