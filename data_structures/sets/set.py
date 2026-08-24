6a89932beaa1014c57b4fa15e185a4679f3340e9
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 11:03:46 +0530

    add: create set.py for set operations and unique score calculation

---
+++ data_structures/sets/set.py
@@ -0,0 +1,10 @@
+empty_set=set()
+print(empty_set)
+
+number={1,2,2,3,4,5}
+print(number)
+print(len(number))
+
+scores=[90,89,85,85,90,95,100]
+unique_scores=set(scores)
+print(f"unique_scores : {unique_scores}")
