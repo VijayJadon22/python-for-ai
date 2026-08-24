238693803041fb1a97984669816a6b285870beab
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 22:00:19 +0530

    add: implement functions for sum and area calculations in function_return.py

---
+++ learning_functions/function_return.py
@@ -0,0 +1,15 @@
+
+
+def calculate_sum(a,b):
+    return a+b
+
+sum=calculate_sum(a=5,b=4)
+print(sum)
+
+
+def calculate_area(length,width):
+    area=length*width
+    return area
+
+area=calculate_area(length=10,width=30)
+print(area)
