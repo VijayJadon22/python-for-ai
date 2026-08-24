71ce729688cb853a171a6927eafe69d9832f6e35
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 22:37:16 +0530

    refactor: update import statements and remove unused code in base.py

--- import_packages/base.py
+++ import_packages/base.py
@@ -1,4 +1,10 @@
-import math
+# import math
 
-root=math.sqrt(9)
+# root=math.sqrt(9)
+# print(root)
+
+#2nd way to import only the selective functions 
+from math import sqrt,pi
+
+root=sqrt(25)
 print(root)
