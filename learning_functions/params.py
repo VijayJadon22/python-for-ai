381c9dd1c061b4f051fbcf5a23ccebd08e9a936c
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 12:24:26 +0530

    add: implement weather check and greeting functions in params.py

---
+++ learning_functions/params.py
@@ -0,0 +1,18 @@
+def check_weather(temp):
+    if temp>25:
+        print("Its Hot!")
+    elif temp<=20:
+        print("Its Moderate!")
+    else:
+        print("Its Cold!")
+
+check_weather(22)
+
+def greet_vijay(name):
+    print(f"Hello {name}")
+
+greet_vijay(name="jatin")
+def greet_user(first_name,last_name):
+    print(f"Hello {first_name} {last_name}")
+
+greet_user("Jatin","Gahlot")
