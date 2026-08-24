566d327cd4e95e8e90bf5133f056e3b5cecf6691
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-08-24 12:00:07 +0530

    add: implement weather check function in hello.py

--- learning_functions/hello.py
+++ learning_functions/hello.py
@@ -11,3 +11,14 @@ say_goodbye()
 
 for i in range(5):
     say_goodbye()
+
+def check_weather():
+    temperature=35
+    if temperature>30:
+        print("Its Hot!")
+    elif temperature<=25 and temperature >=20:
+        print("Its Moderate!")
+    else: 
+        print("Its Cold!")
+
+check_weather()
