9b3fa193eb4e5acae9833ca9fea08eaa65531d43
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-09 17:25:15 +0530

    add: save Paris weather data to CSV and create data directory if it doesn't exist

--- get_data/get_data.py
+++ get_data/get_data.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime, timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 #calculate dates
 today=datetime.now()
@@ -56,3 +57,11 @@ plt.tight_layout()
 # Save the plot
 plt.savefig('weather_chart.png')
 plt.show()
+
+#Create data folder if does not exist
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+#Save to CSV
+df.to_csv("data/paris_weather.csv",index=False)
+print("Data saved to CSV")
