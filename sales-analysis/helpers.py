1ede181b1c4cd36f04b417bf57bf08eca271cb49
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-10 15:53:00 +0530

    add: implement helpers.py with functions to calculate total and format currency

---
+++ sales-analysis/helpers.py
@@ -0,0 +1,9 @@
+
+def calculate_total(quantity,price):
+    """Calculate Total for a Single Item"""
+    return quantity*price
+
+def format_currency(amount):
+    """Format number as currency"""
+    return f"${amount:,.2f}"
+
